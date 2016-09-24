261ad337b63a67330727d961949340119a110dd1
vagrant <vagrant@homestead>
2016-09-24 17:47:46 +0800

    init project

---
+++ src/MD5Hasher.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thirdriver
+ * Date: 16/9/24
+ * Time: 下午2:32
+ */
+
+namespace Laravist\Hasher;
+
+
+/**
+ * Class MD5Hasher
+ * @package Laravist\Hasher
+ */
+class MD5Hasher
+{
+    /**
+     * @param $value
+     * @param array $options
+     * @return string
+     */
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ?: '';
+
+        return hash('md5', $value . $salt);
+    }
+
+    /**
+     * @param $value
+     * @param $hashValue
+     * @param array $options
+     * @return bool
+     */
+    public function check($value, $hashValue, array $options = [])
+    {
+        $salt = isset($options['salt']) ?: '';
+
+        return hash('md5', $value . $salt) === $hashValue;
+    }
+}
